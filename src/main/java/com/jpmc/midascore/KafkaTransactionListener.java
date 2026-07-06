73959507412bc8ef32c0e65eb7c433fbba3325b5
Owen871 <owen.gagnon@vikings.berry.edu>
2026-07-06 18:11:04 -0400

    1
    1

---
+++ src/main/java/com/jpmc/midascore/KafkaTransactionListener.java
@@ -0,0 +1,15 @@
+package com.jpmc.midascore;
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaTransactionListener {
+    @KafkaListener(
+        topics = "${general.kafka-topic}",
+        groupId = "midas-core")
+    public void recieveTransaction(Transaction transaction){
+
+        System.out.println(transaction);
+    }
+}
